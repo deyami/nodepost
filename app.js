4ff46b92dbfa81110a958d6824791d9c5d1752e8
tongfeng.dhy <tongfeng.dhy@taobao.com>
2015-09-15 10:15:32 +0800

    修改程序页面

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 'use strict';
-var koa = require('koa');
+
 var _ = require('koa-route');
 var session = require('koa-session');
 var serve = require('koa-static');
-var app = koa();
+var Controller = require('./app/controller/controller');
+var app = require('xtpl/lib/koa')(require('koa')(),{
+    views:'./app/views'
+});
 
+//var app= require('koa')();
 app.keys = "keys";
 app.use(session({
     "key":'a_token',
@@ -12,7 +16,10 @@ app.use(session({
 },app));
 
 app.use(serve(__dirname + '/app/statics'));
-app.use(serve(__dirname + '/app/views'));
-//app.use(_.get('/index.htmll', pets.list));
+//app.use(serve(__dirname + '/app/views'));
+
+
+app.use(_.get('/login',Controller.login));
+app.use(_.get('/maptest',Controller.maptest));
 
 module.exports = app;
---
+++ app/controller/controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    "login": function
+        *() {
+    }
+    ,
+    "maptest": function
+        *() {
+        yield this.render('maptest',{});
+    }
+
+}
